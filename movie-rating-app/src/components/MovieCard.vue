b08c94dee40547a76811158c54765041b6d7c25d
Tim Rayner <tim.rayner@liftshare.com>
2023-08-11 15:09:23 +0100

    movie-rating-app: updated UI for movie card

--- movie-rating-app/src/components/MovieCard.vue
+++ movie-rating-app/src/components/MovieCard.vue
@@ -21,6 +21,10 @@ const stars = computed(() => {
   const numStars = Math.floor(props.movie.rating);
   return Array(numStars).fill("");
 });
+
+const purchase = () => {
+  console.log("purchase");
+};
 </script>
 
 <template>
@@ -36,9 +40,7 @@ const stars = computed(() => {
   <div
     class="flip-card bg-transparent w-[25vw] h-[65vh] hover:cursor-pointer mx-2"
   >
-    <div
-      class="flip-card-inner relative w-full h-full text-center transition-transform"
-    >
+    <div class="flip-card-inner relative w-full h-full transition-transform">
       <!-- FRONTSIDE -->
       <div
         class="flip-card-front box-shadow-md absolute flex flex-col justify-center w-full h-full backface-hidden border border-solid border-coral rounded-lg"
@@ -52,8 +54,8 @@ const stars = computed(() => {
           <div
             class="overlay absolute top-0 left-0 right-0 h-full w-full rounded-lg content-[''] bg-black z-20 opacity-20"
           ></div>
-          <div class="content z-40 relative w-full h-full m-auto">
-            <div class="ratings flex text-center items-center w-fit m-auto">
+          <div class="content z-40 relative w-full h-full">
+            <div class="ratings flex text-center items-center w-fit m-2">
               <span v-for="star in stars">
                 <StarIcon class="star" />
               </span>
@@ -63,10 +65,24 @@ const stars = computed(() => {
       </div>
       <!-- BACKSIDE-->
       <div
-        class="flip-card-back box-shadow-md absolute flex flex-col justify-center w-full h-full backface-hidden border border-solid border-coral rounded-lg"
+        class="flip-card-back box-shadow-md absolute flex flex-col w-full h-full backface-hidden border border-solid border-coral rounded-lg"
       >
-        <p class="title">BACK</p>
-        <p>Leave Me</p>
+        <div class="ratings flex text-center items-center w-fit m-2">
+          <span v-for="star in stars">
+            <StarIcon class="star" />
+          </span>
+        </div>
+        <div class="content mx-3 z-50">
+          <h3 class="text-2xl">{{ movie.name }}</h3>
+          <p class="text-lg">{{ movie.description }}</p>
+          <button
+            v-if="movie.inTheaters"
+            @click="purchase()"
+            class="button p-3 bg-purple-600 rounded-lg mt-3"
+          >
+            BOOK TICKETS
+          </button>
+        </div>
       </div>
     </div>
   </div>
--- movie-rating-app/src/views/Home.vue
+++ movie-rating-app/src/views/Home.vue
@@ -7,7 +7,7 @@ import MovieCard from "@/components/MovieCard.vue";
 const movies = ref<Movie[]>(items as Movie[]);
 </script>
 <template>
-  <div class="flex movies my-24">
+  <div class="flex movies my-44">
     <div v-for="movie in movies" class="">
       <MovieCard :movie="movie" />
     </div>
