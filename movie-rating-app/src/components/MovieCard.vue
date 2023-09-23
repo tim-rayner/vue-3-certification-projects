7b3b4538a614c7bb69ac076be4b89094ad07f31a
Tim Rayner <tim.rayner2020@gmail.com>
2023-09-23 13:42:36 +0100

    refactor: made movie cards w/h responsive

--- movie-rating-app/src/components/MovieCard.vue
+++ movie-rating-app/src/components/MovieCard.vue
@@ -30,79 +30,81 @@ const shortenDescription = (lngDescription: string) => {
 </script>
 
 <template>
-  <VueFlip
-    width="25vw"
-    height="55vh"
-    :active-hover="true"
-    class="hover:cursor-pointer mx-3 w-auto rounded-lg"
-  >
-    <!-- FRONT -->
-    <template v-slot:front>
-      <div class="content rounded-lg p-2 h-full relative shadow-xl">
-        <div
-          class="overlay absolute b-0 top-0 left-0 right-0 w-full h-full rounded-lg bg-black z-20 opacity-60"
-        ></div>
-        <div
-          class="poster absolute b-0 top-0 left-0 right-0 w-full h-full rounded-lg bg-black z-10"
-        >
-          <img
-            class="w-full h-full object-cover rounded-lg"
-            :src="movie.image"
-            alt="poster"
-          />
-        </div>
-
-        <div class="content z-50 relative">
-          <div class="title text-4xl font-semibold text-white">
-            {{ movie.name }}
-          </div>
-          <div class="stars-wrapper flex flex-row my-1">
-            <div class="star" v-for="star in stars">
-              <StarIcon class="h-6 w-6 mr-1 text-yellow-400" :key="star" />
-            </div>
-          </div>
-          <div class="description text-white text-lg">
-            {{ shortenDescription(movie.description) }}
-          </div>
-        </div>
-      </div>
-    </template>
-    <!-- BACK -->
-    <template v-slot:back>
-      <div class="content rounded-lg p-2 h-full relative shadow-xl">
-        <div
-          class="overlay absolute b-0 top-0 left-0 right-0 w-full h-full rounded-lg bg-black z-20 opacity-60"
-        ></div>
-        <div class="content z-50 relative">
-          <div class="title text-4xl font-semibold text-white">
-            {{ movie.name }}
-          </div>
-          <div class="stars-wrapper flex flex-row my-1">
-            <div class="star" v-for="star in stars">
-              <StarIcon class="h-6 w-6 mr-1 text-yellow-400" :key="star" />
-            </div>
-          </div>
-          <div class="description text-white text-lg">
-            {{ movie.description }}
-          </div>
-          <div class="purchase-btn-wrapper flex flex-row">
-            <button
-              class="purchase-btn bg-yellow-400 text-black rounded-lg px-4 py-2 mt-2"
-              @click="purchase"
-              v-if="movie.inTheaters"
-            >
-              Book Tickets
-            </button>
-            <button
-              class="purchase-btn bg-orange-400 text-black rounded-lg px-4 py-2 mt-2"
-              @click="watchTrailer"
-            >
-              Watch Trailer
-            </button>
-          </div>
-        </div>
-      </div></template
+  <div class="w-[25vw] h-[55vh] relative mx-3">
+    <VueFlip
+      width="100%"
+      height="100%"
+      :active-hover="true"
+      class="hover:cursor-pointer w-auto rounded-lg"
     >
-  </VueFlip>
+      <!-- FRONT -->
+      <template v-slot:front>
+        <div class="content rounded-lg p-2 h-full relative shadow-xl">
+          <div
+            class="overlay absolute b-0 top-0 left-0 right-0 w-full h-full rounded-lg bg-black z-20 opacity-60"
+          ></div>
+          <div
+            class="poster absolute b-0 top-0 left-0 right-0 w-full h-full rounded-lg bg-black z-10"
+          >
+            <img
+              class="w-full h-full object-cover rounded-lg"
+              :src="movie.image"
+              alt="poster"
+            />
+          </div>
+
+          <div class="content z-50 relative">
+            <div class="title text-4xl font-semibold text-white">
+              {{ movie.name }}
+            </div>
+            <div class="stars-wrapper flex flex-row my-1">
+              <div class="star" v-for="star in stars">
+                <StarIcon class="h-6 w-6 mr-1 text-yellow-400" :key="star" />
+              </div>
+            </div>
+            <div class="description text-white text-lg">
+              {{ shortenDescription(movie.description) }}
+            </div>
+          </div>
+        </div>
+      </template>
+      <!-- BACK -->
+      <template v-slot:back>
+        <div class="content rounded-lg p-2 h-full relative shadow-xl">
+          <div
+            class="overlay absolute b-0 top-0 left-0 right-0 w-full h-full rounded-lg bg-black z-20 opacity-60"
+          ></div>
+          <div class="content z-50 relative">
+            <div class="title text-4xl font-semibold text-white">
+              {{ movie.name }}
+            </div>
+            <div class="stars-wrapper flex flex-row my-1">
+              <div class="star" v-for="star in stars">
+                <StarIcon class="h-6 w-6 mr-1 text-yellow-400" :key="star" />
+              </div>
+            </div>
+            <div class="description text-white text-lg">
+              {{ movie.description }}
+            </div>
+            <div class="purchase-btn-wrapper flex flex-row">
+              <button
+                class="purchase-btn bg-yellow-400 text-black rounded-lg px-4 py-2 mt-2"
+                @click="purchase"
+                v-if="movie.inTheaters"
+              >
+                Book Tickets
+              </button>
+              <button
+                class="purchase-btn bg-orange-400 text-black rounded-lg px-4 py-2 mt-2"
+                @click="watchTrailer"
+              >
+                Watch Trailer
+              </button>
+            </div>
+          </div>
+        </div></template
+      >
+    </VueFlip>
+  </div>
 </template>
 <style scoped lang="scss"></style>
