c4d70222ae468149a912133371b19e2c69a41bbb
Tim Rayner <tim.rayner2020@gmail.com>
2023-09-23 15:00:12 +0100

    basic ui updates

--- movie-rating-app/src/components/Navbar.vue
+++ movie-rating-app/src/components/Navbar.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import profilePicture from "@/assets/pp.jpg";
 
 const menuToggled = ref(false);
 </script>
@@ -135,7 +136,7 @@ const menuToggled = ref(false);
                 <span class="sr-only">Open user menu</span>
                 <img
                   class="h-8 w-8 rounded-full"
-                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  :src="profilePicture"
                   alt=""
                 />
               </button>
--- movie-rating-app/src/views/Home.vue
+++ movie-rating-app/src/views/Home.vue
@@ -7,7 +7,13 @@ import MovieCard from "@/components/MovieCard.vue";
 const movies = ref<Movie[]>(items as Movie[]);
 </script>
 <template>
-  <div class="flex movies overflow-x-scroll no-scrollbar scrollbar-draggable">
+  <h2 class="font-bold text-4xl m-4 mb-2">Popular</h2>
+  <h4 class="font-normal text-lg m-4 mt-2 underline hover:cursor-pointer">
+    View all...
+  </h4>
+  <div
+    class="flex flex-row movies overflow-x-scroll no-scrollbar scrollbar-draggable pt-2"
+  >
     <div v-for="movie in movies" class="">
       <MovieCard :movie="movie" />
     </div>
