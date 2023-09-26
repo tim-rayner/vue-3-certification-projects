1428c6614ec4fa4ec5120e99ac3c377a1cb6dbc1
Tim Rayner <tim.rayner2020@gmail.com>
2023-09-26 23:37:16 +0100

    update: updated movie rating star to display a underscore if unrated

--- movie-rating-app/src/components/MovieCard.vue
+++ movie-rating-app/src/components/MovieCard.vue
@@ -87,7 +87,7 @@ const updateRating = (newRating: number) => {
               <p
                 class="text-black z-50 text-3xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
               >
-                {{ movie.rating }}
+                {{ movie.rating === 0 ? "-" : movie.rating }}
               </p>
             </div>
           </div>
