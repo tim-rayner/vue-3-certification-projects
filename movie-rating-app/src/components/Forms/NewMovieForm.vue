8ca35b2a2d88acd6867adcba9770df064f6588d3
Tim Rayner <tim.rayner2020@gmail.com>
2023-09-30 01:02:22 +0100

    update: added todo list

--- movie-rating-app/src/components/Forms/NewMovieForm.vue
+++ movie-rating-app/src/components/Forms/NewMovieForm.vue
@@ -1,4 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+/**
+ * @TODO add the following form items
+ *  - Title
+ *  - Description
+ *  - Initial Rating
+ *  - Image URL
+ *  - Trailer URL
+ *  - Release Date
+ *  - Genre
+ */
+
+/**
+ * @OTHERTODOS add the following nice to haves
+ *  - add form validation
+ *  - add CSV bulk upload
+ *  - embed trailer in iframe
+ */
+</script>
 
 <template>
   <form></form>
